87f74d7be9459004ca71b7b1e90f3210d56eba00
Dan Cross <dancross96@gmail.com>
2022-08-01 11:45:07 -0500

    used the assign attribute instead of assigning each attribute one at a time in the PATCH action

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -21,11 +21,17 @@ class ShoesController < ApplicationController
 
   def update
     shoe = Shoe.find_by(id: params[:id])
-    shoe.brand = params[:brand] || shoe.brand
-    shoe.color = params[:color] || shoe.color
-    shoe.model = params[:model] || shoe.model
-    shoe.style = params[:style] || shoe.style
-    shoe.save
+    # shoe.brand = params[:brand] || shoe.brand
+    # shoe.color = params[:color] || shoe.color
+    # shoe.model = params[:model] || shoe.model
+    # shoe.style = params[:style] || shoe.style
+    # shoe.save
+    shoe.assign_attributes(
+      brand: params[:brand] || shoe.brand,
+      color: params[:color] || shoe.color,
+      model: params[:model] || shoe.model,
+      style: params[:style] || shoe.style
+    )
     render json: shoe
   end
 
